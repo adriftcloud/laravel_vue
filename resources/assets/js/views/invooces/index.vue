f1ea2e2ecde75a3823ea629137456d9fa9912bb7
sonoma889 <niz.tseng@gmail.com>
2017-12-22 16:57:17 +0800

    樣式完成 首頁列表完成　分頁

--- resources/assets/js/views/invooces/index.vue
+++ resources/assets/js/views/invooces/index.vue
@@ -22,7 +22,7 @@
                 </thead>
 
                 <tbody>
-                    <tr v-for="item in model.data" :key="item.data">
+                    <tr v-for="item in model.data" :key="item.data" @click="detailsPage(item)">
                         <td class="w-1">{{ item.id }}</td>
                         <td class="w-3">{{ item.date }}</td>
                         <td class="w-3">{{ item.number }}</td>
@@ -33,7 +33,18 @@
                 </tbody>
             </table>
         </div>
-        <div class="panel-foot"></div>
+        <div class="panel-foot flex-between">
+            <div>
+                <small>
+                    Showing {{ model.from }} - {{ model.to }} of {{ model.total}}
+                </small>
+            </div>
+
+            <div>
+                <button class="btn btn-sm" :disabled="!model.prev_page_url" @click="prevPage">Prev</button>
+                <button class="btn btn-sm" :disabled="!model.next_page_url" @click="nextPage">Next</button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -51,13 +62,13 @@
             }
         },
         beforeRouteEnter(to, from, next) {
-            get('/api/invoices')
+            get('/api/invoices', to.query)
                 .then((res) => {
                     next(vm => vm.setData(res))
                 })
         },
         beforeRouteUpdate(to, from, next) {
-            get('/api/invoices')
+            get('/api/invoices', to.query)
                 .then((res) => {
                     this.setData(res)
                     next()
@@ -67,7 +78,33 @@
             setData(res) {
                 Vue.set(this.$data, 'model', res.data.results)
                 this.page = this.model.current_page
+            },
+            detailsPage(item) {
+                this.$router.push(`/invoices/${item.id}`)
+            },
+            nextPage() {
+                if (this.model.next_page_url) {
+                    const query = Object.assign({}, this.$route.query)
+                    query.page = query.page ? (Number(query.page) + 1) : 2
+
+                    this.$router.push({
+                        path: '/invoices',
+                        query: query
+                    })
+                }
+            },
+            prevPage() {
+                if (this.model.prev_page_url) {
+                    const query = Object.assign({}, this.$route.query)
+                    query.page = query.page ? (Number(query.page) - 1) : 1
+
+                    this.$router.push({
+                        path: '/invoices',
+                        query: query
+                    })
+                }
             }
+
         }
     }
 
--- resources/assets/js/views/invooces/show.vue
+++ resources/assets/js/views/invooces/show.vue
@@ -1,5 +1,94 @@
 <template>
-    <div>
-        Show
+    <div class="panel" v-if="show">
+
+        <div class="panel-heading">
+            <div class="panel-title">{{ model.number}}</div>
+            <div>
+                <router-link to="/invoices" class="btn">Back</router-link>
+                <router-link to="`/invoices/${model.id}/edit`" class="btn">Edit</router-link>
+                <button class="btn btn-error">Delete</button>
+            </div>
+        </div>
+        <div class="panel-body">
+            <div class="document">
+                <div class="row">
+                    <div class="col-6">
+                        <strong>To:</strong>
+                        <div>
+                            <span>{{ model.customer.text }}</span>
+                            <pre>{{ model.customer.address }}</pre>
+                        </div>
+                    </div>
+
+                    <div class="col-6 col-offset-12">
+                        <table class="document-summary">
+                            <tbody>
+                                <tr>
+                                    <td colspan="2">
+                                        <span class="document-title">INVOICE</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Number</td>
+                                    <td>{{ model.number }}</td>
+                                </tr>
+                                <tr>
+                                    <td>Date</td>
+                                    <td>{{ model.date }}</td>
+                                </tr>
+                                <tr>
+                                    <td>Due Date</td>
+                                    <td>{{ model.due_date }}</td>
+                                </tr>
+                                <tr v-if="model.reference">
+                                    <td>Reference</td>
+                                    <td>{{ model.reference }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+
     </div>
 </template>
+
+<script type="text/javascript">
+    import Vue from 'vue'
+    import {get, byMethod} from '../../lib/api'
+
+    export default {
+        data() {
+            return {
+                show: false,
+                model: {
+                    items: [],
+                    customer: {}
+                }
+            }
+        },
+        beforeRouteEnter(to, from, next) {
+            get(`/api/invoices/${to.params.id}`)
+                .then((res) => {
+                    next(vm => vm.setData(res))
+                })
+        },
+        beforeRouteUpdate(to, from, next) {
+            this.show = false
+            get(`/api/invoices/${to.params.id}`)
+                .then((res) => {
+                    this.setData(res)
+                    next()
+                })
+        },
+        methods: {
+            setData(res) {
+                Vue.set(this.$data, 'model', res.data.model)
+                this.show = true
+            }
+        }
+    }
+</script>
